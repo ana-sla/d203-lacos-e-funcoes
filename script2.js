b5e0492925fa3085517432f8ec8cc38a75876f16
ana <anapntra@gmail.com>
2019-09-12 11:58:49 -0300

    exercicio com math para gerar numero aleatorio

--- script2.js
+++ script2.js
@@ -1,7 +1,6 @@
 // function somar(n1,n2){ //esse entre parenteses sao os paramentros da função, os elementos q ela vai receber
 //     let soma: n1 + n2;
 //     return soma;
-}
 
 function FazerEscada(material, qtdDegraus){
 // let material = prompt("digite o material");
@@ -12,4 +11,16 @@ for(let i = 0; i < qtdDegraus; i++){
     escada += material;
     console.log(escada);
 }
+}
+
+function somarinfinito (){
+    let somaa =0
+    for(let i = 0; i<arguments.length;i++){
+        somaa += arguments[i]
+        return somaa;
+    }
+}
+
+const gerarAleatorio= (min, max) => {
+    return Math.floor(Math.random()*(max - min +1)) + min
 }
